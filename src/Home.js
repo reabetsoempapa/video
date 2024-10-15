3d91494d5ac3382fb0cc8837deaef3a196b8bbd2
Reabetsoe Mpapa <reabetsoempapa44@gmail.com>
2024-10-15 23:11:47 +0200

    Set up Project: Basics of Routing

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ color: "black" }}>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
